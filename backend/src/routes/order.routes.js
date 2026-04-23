c736b7ae8f6cf134c6f673f6639cacbb5acb997e
Kunal Raikwar <kunalraikwar12367@gmail.com>
2026-04-23 21:43:07 +0530

    remove extra code

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,17 +1,3 @@
-// // ==================== order.routes.js ====================
-// const express = require("express");
-// const router = express.Router();
-// const { placeOrder, getMyOrders, getOrderById } = require("../controllers/order.controller");
-// const { protect } = require("../middleware/auth.middleware");
-
-// router.post("/", placeOrder);                    // Guest bhi order kar sakta hai
-// router.get("/my", protect, getMyOrders);         // Login hona zaruri
-// router.get("/:id", protect, getOrderById);
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const router = express.Router();
 const { placeOrder, getMyOrders, getOrderById } = require("../controllers/order.controller");
